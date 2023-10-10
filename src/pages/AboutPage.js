1ee564715fd5815509f34cd63c53230f6d133a63
larimos <larisa.miriana@gmail.com>
2023-10-10 00:23:45 -0400

    Fixed second build error

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -21,9 +21,9 @@ function AboutPage() {
         <div className="text-xl lg:text-3xl md:text-2xl sm:text-xl font-archivio leading-6 lg:leading-8">
          <span className="font-chomsky md:text-4xl lg:text-6xl sm:text-xl">The New York Times</span> ARTICLE SEARCH
         </div>
-        <div className='flex flex-row items-center mt-8 p-4'>
-        <img src={Arrow} alt="Arrow pointing down" className="hover-target" onClick={scrollToNextDiv} />
-        <div className='hover-target font-archivio text-lg ml-8' onClick={scrollToNextDiv}>Scroll Down</div>
+        <div className="absolute bottom-40 left-8 flex flex-col items-center font-archivio">
+          <div className="hover-target text-lg mb-2" onClick={scrollToNextDiv}>Slide Down</div>
+          <img src={Arrow} alt="Arrow pointing down" className="hover-target"  onClick={scrollToNextDiv} />
         </div>
       </div> 
       <div ref={targetRef} className="relative flex flex-col justify-center align-center z-10 bg-teal-50 py-32"> {/* Removed h-full and added pb-12 for some padding at the bottom */}
